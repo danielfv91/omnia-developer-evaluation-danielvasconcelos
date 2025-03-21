6e345f52ab0d46e05ed1bca170cefd7e6aa7bd3b
Daniel Vasconcelos <danielfelipe91@gmail.com>
2025-03-21 11:12:11 -0300

    feat: criação das entidades Sale e SaleItem e migration inicial

---
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+public class Sale
+{
+    public Guid Id { get; set; }
+    public int SaleNumber { get; set; }
+    public DateTime SaleDate { get; set; }
+    public Guid CustomerId { get; set; }
+    public string CustomerName { get; set; }
+    public decimal TotalAmount { get; set; }
+    public string Branch { get; set; }
+    public bool IsCancelled { get; set; }
+
+    public List<SaleItem> Items { get; set; } = new();
+}
+
+public class SaleItem
+{
+    public Guid Id { get; set; }
+    public Guid SaleId { get; set; }
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal DiscountPercentage { get; set; }
+    public decimal TotalItemAmount { get; set; }
+    public bool IsCancelled { get; set; }
+}

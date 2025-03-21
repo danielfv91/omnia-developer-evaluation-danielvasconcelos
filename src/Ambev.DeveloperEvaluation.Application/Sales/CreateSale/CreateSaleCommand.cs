4a043f2afc012b8e3952e89063a71945ee7704a0
Daniel Vasconcelos <danielfelipe91@gmail.com>
2025-03-21 12:43:06 -0300

    feat: implementar CreateSale completo com MediatR, AutoMapper e FluentValidation

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale
+{
+    public class CreateSaleCommand : IRequest<CreateSaleResult>
+    {
+        public int SaleNumber { get; set; }
+        public DateTime SaleDate { get; set; }
+        public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Branch { get; set; }
+        public List<CreateSaleItemDto> Items { get; set; }
+    }
+
+    public class CreateSaleItemDto
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/CreateSale/CreateSaleValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.CreateSale
+{
+    public class CreateSaleValidator : AbstractValidator<CreateSaleCommand>
+    {
+        public CreateSaleValidator()
+        {
+            RuleFor(s => s.SaleNumber).GreaterThan(0);
+            RuleFor(s => s.CustomerId).NotEmpty();
+            RuleFor(s => s.CustomerName).NotEmpty().MaximumLength(100);
+            RuleFor(s => s.Branch).NotEmpty().MaximumLength(50);
+
+            RuleForEach(s => s.Items).SetValidator(new CreateSaleItemValidator());
+        }
+    }
+
+    public class CreateSaleItemValidator : AbstractValidator<CreateSaleItemDto>
+    {
+        public CreateSaleItemValidator()
+        {
+            RuleFor(i => i.ProductId).NotEmpty();
+            RuleFor(i => i.ProductName).NotEmpty().MaximumLength(100);
+            RuleFor(i => i.Quantity).InclusiveBetween(1, 20);
+            RuleFor(i => i.UnitPrice).GreaterThan(0);
+        }
+    }
+}
